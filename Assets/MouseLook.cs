bdf52cd5f042a9d2cee836b6f7504510773b8a54
SamB <sambolduc4@gmail.com>
2021-02-23 00:48:54 -0500

    Add colliders, player (First person), player movement, sound (walk, jump), head & walk blob, gravity

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * this.mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * this.mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        this.playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
