851b731c8e4fa2e344284adda1ebf81a123f19ce
Simon Bolduc <36117510+simbolduc@users.noreply.github.com>
2021-04-23 03:59:44 -0400

    WIP

---
+++ Assets/Scripts/Game/GameActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameActions : MonoBehaviour
+{
+
+    public Canvas escMenu;
+    public Canvas parameters;
+
+    private void Start()
+    {
+        parameters.gameObject.SetActive(false);
+    }
+
+    public void ShowParameters()
+    {
+        escMenu.gameObject.SetActive(false);
+        Cursor.lockState = !parameters.gameObject.activeSelf ? CursorLockMode.None : CursorLockMode.Confined;
+        parameters.gameObject.SetActive(!parameters.gameObject.activeSelf);
+    }
+
+    public void SaveParameters()
+    {
+        HotkeysManager.Get().Save();
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        if (audioManager != null)
+        {
+            audioManager.SavePreferences();
+        }
+
+        Cursor.lockState = CursorLockMode.Confined;
+        parameters.gameObject.SetActive(false);
+    }
+
+}
